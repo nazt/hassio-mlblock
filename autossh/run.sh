fcfc58dac9785aa8387975a53eb8b895dd8457a4
Nat Weerawan <nat.wrw@gmail.com>
2021-07-09 10:44:58 +0700

    Update autossh

--- autossh/run.sh
+++ autossh/run.sh
@@ -19,6 +19,20 @@ AUTOSSH_ARGS="-M $MONITOR_PORT "
 SSH_ARGS="-nNTv -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i $KEY_PATH -R $TUNNEL_REMOTE_STRING $TUNNEL_USER@$TUNNEL_HOST"
 DAEMON_ARGS=" $AUTOSSH_ARGS $SSH_ARGS"
 
+# Set username and password for the broker
+for remote in $(bashio::config 'tunnel_remotes|keys'); do
+  bashio::config.require.remote_string "tunnel_remotes[${remote}].remote_string"
+#   bashio::config.require.password "tunnel_remotes[${remote}].password"
+  remote_string=$(bashio::config "tunnel_remotes[${remote}].remote_string")
+#   password=$(bashio::config "tunnel_remotes[${remote}].password")
+
+  bashio::log.info "GOT Remote: ${remote_string}"
+#   password=$(np -p "${password}")
+#   echo "${username}:${password}" >> "${PW}"
+#   echo "user ${username}" >> "${ACL}"
+done
+
+
 echo "#!/usr/bin/env bashio" > go.sh
 echo "AUTOSSH_DEBUG=1 autossh" "$DAEMON_ARGS" >> go.sh
 cat go.sh
