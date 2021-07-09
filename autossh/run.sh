e0f0bb1bb9f7b4cd4f2c7187029da8001281ac6c
Nat Weerawan <nat.wrw@gmail.com>
2021-07-09 10:31:43 +0700

    Update configuration

--- autossh/run.sh
+++ autossh/run.sh
@@ -7,15 +7,16 @@ bashio::log.info "Public key:"
 
 cat /root/.ssh/id_rsa.pub
 
-TUNNEL_HOST=reverse-proxy.archtomation.com
+TUNNEL_HOST=$(bashio::config 'tunnel_host')
 TUNNEL_USER=device
 TUNNEL_PORT=55001
 MONITOR_PORT=0
 KEY_PATH=/root/.ssh/id_rsa
+TUNNEL_REMOTE_STRING=$(bashio::config 'tunnel_remote_string')
 
 
 AUTOSSH_ARGS="-M $MONITOR_PORT "
-SSH_ARGS="-nNTv -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i $KEY_PATH -R $TUNNEL_PORT:localhost:8123 $TUNNEL_USER@$TUNNEL_HOST"
+SSH_ARGS="-nNTv -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i $KEY_PATH -R $TUNNEL_REMOTE_STRING $TUNNEL_USER@$TUNNEL_HOST"
 DAEMON_ARGS=" $AUTOSSH_ARGS $SSH_ARGS"
 
 echo "#!/usr/bin/env bashio" > go.sh
