f3fceb86a5102ab64903a336b1b59378550c6776
Nat Weerawan <nat.wrw@gmail.com>
2021-07-09 11:02:08 +0700

    Add multiple endpoint

--- autossh/run.sh
+++ autossh/run.sh
@@ -14,12 +14,11 @@ TUNNEL_USER=device
 TUNNEL_PORT=55001
 MONITOR_PORT=0
 KEY_PATH=/root/.ssh/id_rsa
-TUNNEL_REMOTE_STRING=$(bashio::config 'tunnel_remote_string')
+# TUNNEL_REMOTE_STRING=$(bashio::config 'tunnel_remote_string')
 
 
-AUTOSSH_ARGS="-M $MONITOR_PORT "
-SSH_ARGS="-nNTv -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i $KEY_PATH -R $TUNNEL_REMOTE_STRING $TUNNEL_USER@$TUNNEL_HOST"
-DAEMON_ARGS=" $AUTOSSH_ARGS $SSH_ARGS"
+echo "#!/usr/bin/env bashio" > go.sh
+echo "AUTOSSH_DEBUG=1 autossh" "$DAEMON_ARGS" >> go.sh
 
 # Set username and password for the broker
 for remote in $(bashio::config 'tunnel_remotes|keys'); do
@@ -27,19 +26,23 @@ for remote in $(bashio::config 'tunnel_remotes|keys'); do
   bashio::log.info "tunnel_remotes[${remote}].remote_string"
 #   bashio::config.require.remote_string "tunnel_remotes[${remote}].remote_string"
 #   bashio::config.require.password "tunnel_remotes[${remote}].password"
-  remote_string=$(bashio::config "tunnel_remotes[${remote}].remote_string")
+  TUNNEL_REMOTE_STRING=$(bashio::config "tunnel_remotes[${remote}].remote_string")
 #   password=$(bashio::config "tunnel_remotes[${remote}].password")
 
-  bashio::log.info "GOT Remote: ${remote_string}"
+  bashio::log.info "GOT Remote: ${TUNNEL_REMOTE_STRING}"
+  AUTOSSH_ARGS="-M $MONITOR_PORT -f "
+#   TUNNEL_REMOTE_STRING=$(bashio::config 'tunnel_remote_string')
+  SSH_ARGS="-nNTv -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i $KEY_PATH -R $TUNNEL_REMOTE_STRING $TUNNEL_USER@$TUNNEL_HOST"
+  DAEMON_ARGS=" $AUTOSSH_ARGS $SSH_ARGS"
+  echo "AUTOSSH_DEBUG=1 autossh" "$DAEMON_ARGS" >> go.sh
+
 #   password=$(np -p "${password}")
 #   echo "${username}:${password}" >> "${PW}"
 #   echo "user ${username}" >> "${ACL}"
 done
 
 
-echo "#!/usr/bin/env bashio" > go.sh
-echo "AUTOSSH_DEBUG=1 autossh" "$DAEMON_ARGS" >> go.sh
+
 cat go.sh
 chmod +x ./go.sh
-
 ./go.sh
