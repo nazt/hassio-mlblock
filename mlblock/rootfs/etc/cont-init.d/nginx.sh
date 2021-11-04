066f30f820e375c32f8f0284f371de4485920dec
Nat W <nat.wrw@gmail.com>
2021-11-04 14:03:02 +0700

    Update port

--- mlblock/rootfs/etc/cont-init.d/nginx.sh
+++ mlblock/rootfs/etc/cont-init.d/nginx.sh
@@ -13,12 +13,12 @@ bashio::var.json \
         -out /etc/nginx/servers/ingress.conf
 
 # Generate direct access configuration, if enabled.
-if bashio::var.has_value "$(bashio::addon.port 3001)"; then
+if bashio::var.has_value "$(bashio::addon.port 80)"; then
     bashio::config.require.ssl
     bashio::var.json \
         certfile "$(bashio::config 'certfile')" \
         keyfile "$(bashio::config 'keyfile')" \
-        port "^$(bashio::addon.port 3001)" \
+        port "^$(bashio::addon.port 80)" \
         ssl "^$(bashio::config 'ssl')" |
         tempio \
             -template /etc/nginx/templates/direct.gtpl \
